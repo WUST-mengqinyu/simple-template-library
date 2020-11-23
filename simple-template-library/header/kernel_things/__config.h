f44dda88cb289720e1199b8cadf085683f8b9aef
mengqinyu <mengqinyu@bytedance.com>
2020-11-23 17:31:11 +0800

    add global config.

---
+++ simple-template-library/header/kernel_things/__config.h
@@ -0,0 +1,23 @@
+//
+// Created by mengqinyu on 2020/11/23.
+//
+
+#ifndef SIMPLE_TEMPLATE_LIBRARY___CONFIG_H
+#define SIMPLE_TEMPLATE_LIBRARY___CONFIG_H
+
+#ifndef _LIB_ABI_VERSION
+#  define _LIB_ABI_VERSION 1
+#endif
+
+#define _LIB_CONCAT(_LIB_X,_LIB_Y) _LIB_X##_LIB_Y
+
+#ifndef _LIB_ABI_NAMESPACE
+# define _LIB_ABI_NAMESPACE _LIB_CONCAT(__,_LIB_ABI_VERSION)
+#endif
+
+#define _LIB_TEMPLATE_STL __attribute__ ((__visibility__("default")))
+#define _LIB_BEGIN_NAMESPACE_STL namespace std { inline namespace _LIB_ABI_NAMESPACE {
+#define _LIB_END_NAMESPACE_STL  } }
+
+#define force_inline __inline__ __attribute__((always_inline))
+#endif //SIMPLE_TEMPLATE_LIBRARY___CONFIG_H
