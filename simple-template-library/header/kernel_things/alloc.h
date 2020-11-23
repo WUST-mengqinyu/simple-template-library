e1a6ebdc5f48e8b5ea7ccf7751fc23c54f778e4d
mengqinyu <mengqinyu@bytedance.com>
2020-11-23 17:32:02 +0800

    add NAMESPACE test.

--- simple-template-library/header/kernel_things/alloc.h
+++ simple-template-library/header/kernel_things/alloc.h
@@ -6,10 +6,13 @@
 #define SIMPLE_TEMPLATE_LIBRARY_ALLOC_H
 
 #pragma once
-#define MIN_ALLOC 0
+#include "__config.h"
+
+
+_LIB_BEGIN_NAMESPACE_STL
 
 extern const unsigned int POINTER_SIZE = sizeof(void *);
-
+extern const int MIN_ALLOC = 0;
 typedef void *memory_resource;
 
 static memory_resource MEMORY_POOL;
@@ -23,5 +26,6 @@ template<typename T>
 alloc<T>::alloc(int _sz) {
 }
 
+_LIB_END_NAMESPACE_STL
 
 #endif //SIMPLE_TEMPLATE_LIBRARY_ALLOC_H
